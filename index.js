da6e1a9c3b10795fe9803cb0e57680fff6187458
DaniOmehe <danielomehe66@gmail.com>
2023-01-03 13:58:36 +0100

    Made finishing touches to inprove responsiveness

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
 //select each element from the DOM
 const searchInput = document.querySelector('#search');
-const searchButton = document.querySelector('.search');
-const searchList = document.getElementById('search-list')
+const searchBox = document.querySelector('.search-box');
+const searchButton = document.querySelector('.search-button');
+const searchList = document.getElementById('search-list');
 const searchListGroup = document.getElementById('search-list-group');
 const newsHeadline = document.getElementById('news-headline');
 const newsContent = document.getElementById('news-content');
 const searchError = document.getElementById('error');
+const menuBtn = document.querySelector('.bars');
+
+let search = false,
+showSearch = false;
+
+menuBtn.addEventListener('click', (e)=>{
+     searchBox.classList.toggle('open-search');
+});
 
 searchInput.addEventListener('focus', (e) => {
-     searchInput.classList.add('expand')
+     if (!search){
+          search = !search;
+          searchInput.classList.add('expand');
+          searchBox.classList.add('expand');
+          searchButton.classList.add('expand');
+     }
+
+     if(window.screen.width <= 768){
+          searchInput.classList.remove('expand');
+          searchBox.classList.remove('expand');
+     }
 });
 
 searchInput.addEventListener('blur', (e) => {
-     if(e.target.value === "" ) searchInput.classList.remove('expand');
+     search = false
+     if(e.target.value === "" ){
+          searchInput.classList.remove('expand');
+          searchBox.classList.remove('expand');
+          searchButton.classList.remove('expand');
+     }
      else e.preventDefault()
 });
-
-//Use fetch to get top headlines 
 
 const getLatestNews = (url) => {
      fetch(url)
      .then(result => result.json())
      .then(data => {
-          console.log(data);
           let {articles} = data;
 
           for(let {publishedAt, source, title, url, urlToImage} of articles){
@@ -32,7 +53,7 @@ const getLatestNews = (url) => {
                newsCardGroup.innerHTML += `
                     <div class="news-card">
                          <div class="news-image">
-                              <img src="${urlToImage}" alt="" class="image">
+                              <img src="${urlToImage}" alt="photo" class="image">
                          </div>
                          <div class="news-card-title">
                               <h2 class="title-text"><a target=_blank href="${url}">${title}</a></h2>
@@ -49,15 +70,27 @@ const getLatestNews = (url) => {
 
 getLatestNews('https://newsapi.org/v2/everything?q=medicine&pageSize=12&apiKey=983b7378865f479eb7304932bbce64e7');
 
-searchButton.addEventListener('click', () => {
+searchButton.addEventListener('click', (e) => {
 
      const searchText = searchInput.value.trim();
 
-     console.log(searchText);
+     if(searchText === '' && !search){
+          search = !search;
+          searchInput.classList.add('expand');
+          searchBox.classList.add('open-search')
+          searchBox.classList.add('expand')
+     }
+     else if(searchText !== ''){
+          search = true
+     }
+     else {
+          search = false
+          searchInput.classList.remove('expand')
+          searchBox.classList.remove('open-search')
+          searchBox.classList.add('expand')
+     }
 
      const totalSearchResults = document.getElementById('total-results');
-
-     console.log(totalSearchResults);
 
      searchList.classList.add('show')
 
@@ -65,7 +98,6 @@ searchButton.addEventListener('click', () => {
           fetch(url)
           .then(result => result.json())
           .then(data => {
-               console.log(data);
 
                totalSearchResults.innerText = `
                Search results for ${searchText}, total results(${data.totalResults})
@@ -95,5 +127,5 @@ searchButton.addEventListener('click', () => {
           });
      };
 
-     getSearchresults(`https://newsapi.org/v2/everything?q=${searchText}&apiKey=983b7378865f479eb7304932bbce64e7`)
+     getSearchresults(`https://newsapi.org/v2/everything?q=${searchText}&apiKey=983b7378865f479eb7304932bbce64e7`);
 });
